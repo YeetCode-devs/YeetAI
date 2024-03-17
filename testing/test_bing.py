2a2958f1db8d9304e05e87a566586eb0e4822998
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2024-03-17 14:52:16 +0800

    Add test for bing
    Change-Id: I0215e3c234960a3eb022bed09d018b11ad5c4b2c

---
+++ testing/test_bing.py
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Copyright (c) 2024, YeetCode Developers <YeetCode-devs@protonmail.com>
+
+from g4f.client import Client
+from g4f.Provider import Bing
+
+
+def generate_response() -> str:
+    client = Client(provider=Bing)
+
+    response = client.chat.completions.create(
+        model="gpt-4.0-turbo",
+        messages=[{"role": "user", "content": "Say hi, with your response starting with START and ending with END"}]
+    )
+
+    return response.choices[0].message.content
+
+
+class TestOutput:
+    def test_output(self):
+        response = generate_response()
+        assert response.startswith("START") and response.endswith("END")
