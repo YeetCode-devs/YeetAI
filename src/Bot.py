41b8fd9c94ae6c2facc0d523946665c2f1171607
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2024-03-19 00:14:27 +0800

    src: Bot: Satisfy type checker

--- src/Bot.py
+++ src/Bot.py
@@ -6,14 +6,14 @@ from os import getenv
 def main() -> None:
     load_dotenv()
 
-    api_id = getenv("API_ID")
-    api_hash = getenv("API_HASH")
-    bot_token = getenv("BOT_TOKEN")
+    api_id = getenv("API_ID", 0)
+    api_hash = getenv("API_HASH", "")
+    bot_token = getenv("BOT_TOKEN", "")
 
     if not all([api_id, api_hash, bot_token]):
         raise ValueError("Could not get all required credentials from env!")
 
-    app = TelegramClient("app", api_id, api_hash).start(bot_token=bot_token)
+    app = TelegramClient("app", int(api_id), api_hash).start(bot_token=bot_token)
 
     @app.on(events.NewMessage(incoming=True, pattern="/start"))
     async def start(event):
