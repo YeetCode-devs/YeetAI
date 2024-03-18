e8c2a13847698025f848d32c73fa0ad30e5b9ec9
pranayadmn <pranayadmn@users.noreply.github.com>
2024-03-18 17:37:19 +0000

    Add license to files

--- scripts/install_hook.py
+++ scripts/install_hook.py
@@ -1,3 +1,19 @@
+# SPDX-License-Identifier: GPL-3.0-only
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Copyright (c) 2024, YeetCode Developers <YeetCode-devs@protonmail.com>
+
 import os
 
 from pathlib import Path
