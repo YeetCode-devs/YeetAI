b40e0a47357b1a226fb27ba17a4d5db847f9c995
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2024-03-19 00:52:26 +0800

    install_hook: Run black using poetry

--- install_hook.py
+++ install_hook.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier
 
+import os
+
 from pathlib import Path
 
 SCRIPT = r"""#!/bin/sh
@@ -7,7 +9,7 @@ set -o noglob
 
 poetry run install-hook
 
-python -m black .
+poetry run black .
 
 for file in $(git diff --cached --name-only); do
     test -f $file && git add $file
@@ -18,4 +20,7 @@ def main() -> None:
     print("Installing pre-commit hook")
     with open(".git/hooks/pre-commit", "w") as f:
         f.write(SCRIPT)
+
+    print(f"Installing dev dependencies (for black)")
+    os.system("poetry install --with dev")
     Path(".git/hooks/pre-commit").chmod(0o700)
