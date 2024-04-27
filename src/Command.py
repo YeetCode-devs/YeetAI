e90b731ecd11aa8017ebc8117e38b557a1fbd586
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2024-04-27 22:11:17 +0800

    src: Command: Implement a more robust mechanism for command-files data

--- src/Command.py
+++ src/Command.py
@@ -18,7 +18,8 @@ import logging
 from importlib import import_module
 from os.path import sep
 from pathlib import Path
-from typing import Callable
+from types import FunctionType
+from typing import Any, Callable
 
 from pyrogram import filters
 from pyrogram.client import Client
@@ -27,13 +28,142 @@ from pyrogram.handlers import MessageHandler
 log: logging.Logger = logging.getLogger(__name__)
 
 
-def load_commands(app: Client) -> list[dict[str, str | Callable]]:
+class CommandData:
+    """A data-structure for a command file's data.
+
+    This class aims to provide type-hinting and a less error-prone implementation. The error-prone
+    parts are now directed towards this class, so the loader implementation does not have to
+    deal with them.
+
+    This includes: Checking whether an important field in the command-file data exists,
+    and checking whether they are of the correct type. In the future, more sanity checks might
+    be implemented, and this class centralizes them.
+
+    Another benefit is that any future changes to the command-file data structure can be
+    implemented here, and the loader implementation does not have to be touched.
+
+    In addition to that, any particular command-file data can be accessed much more easily,
+    without having to verify whether they exist, and they can be accessed with a property-like
+    syntax.
+
+    BONUS: type-hinting and less chance of making mistake - imagine data["name"] or data.get("name")
+    where you can easily make a typo, vs data.name where you can't possibly make any typo.
+    """
+
+    MANDATORY_FIELD: dict = {
+        # field name: required type
+        "name": str,
+        "description": str,
+        "usage": str,
+        "execute": FunctionType,
+    }
+
+    OPTIONAL_FIELD: dict = {
+        # field name: required type
+        "alias": list,
+        "example": str,
+        "category": str,
+    }
+
+    def __init__(self, data: dict, command_path: str):
+        """Data structure for a module's data.
+
+        Args:
+            data (dict): The raw dictionary from the module.
+            command_path (str): Path to the command file. Does not have to be absolute;
+                this is only used for error message.
+        """
+        self._data: dict = data
+        self._command_path: str = command_path
+
+        log.info(f"Verifying command-file '{self.name}' data structure")
+        self.sanity_check()
+
+    def _is_correct_type(self, field_name: str, expected_type: Any) -> None:
+        """Check if the command-file data has the correct type.
+
+        Notes: This is a private method, and thus it is useless to be used outside of this class.
+
+        Args:
+            field_name (str): The key to the dictionary.
+            expected_type (:obj:`Any`): The type of the value of the dictionary's key, aka the type that it should have.
+
+        Raises:
+            ValueError when the field's type does not match the required type.
+        """
+        if type(self._data.get(field_name)) != expected_type:
+            raise ValueError(
+                f"Command '{self._command_path}' implemented the wrong type for '{field_name}' field: "
+                f"Expecting '{expected_type}', got '{type(self._data.get(field_name))}' instead"
+            )
+
+    def sanity_check(self) -> None:
+        """Check if a command file's data is sane."""
+        # First check: Mandatory fields
+        for field_name, required_type in type(self).MANDATORY_FIELD.items():
+            if not self._data.get(field_name):
+                # First we check whether the mandatory field exist at all
+                raise ValueError(
+                    f"Command '{self._command_path}' does not implement the following mandatory field: " f"{field_name}"
+                )
+
+            # Then we make sure it is the correct type
+            self._is_correct_type(field_name, required_type)
+
+        # Second check: Optional fields
+        for field_name, required_type in type(self).OPTIONAL_FIELD.items():
+            # First we check whether the optional field exist at all
+            if self._data.get(field_name):
+                self._is_correct_type(field_name, required_type)
+
+    @property
+    def raw_data(self) -> dict:
+        """Raw data of the command."""
+        return self._data
+
+    @property
+    def name(self) -> str:
+        """Name of the command."""
+        return self._data["name"]
+
+    @property
+    def description(self) -> str:
+        """Description of the command."""
+        return self._data["description"]
+
+    @property
+    def usage(self) -> str:
+        """Usage of the command."""
+        return self._data["usage"]
+
+    @property
+    def execute(self) -> FunctionType:
+        """Function to execute the command."""
+        return self._data["execute"]
+
+    @property
+    def alias(self) -> list[str]:
+        """Aliases of the command. Empty list if there's none."""
+        return self._data.get("alias", [])
+
+    @property
+    def example(self) -> str:
+        """Example of the command. Empty string if there's none."""
+        return self._data.get("example", "")
+
+    @property
+    def category(self) -> str:
+        """Category of the command. Empty string if there's none."""
+        return self._data.get("category", "")
+
+
+def load_commands(app: Client) -> list[CommandData]:
     commands_dir_name: str = "commands"
     bot_src_dir: Path = Path(__file__).parent
     bot_root: Path = bot_src_dir.parent
     commands_dir: Path = Path(bot_src_dir).joinpath(commands_dir_name)
 
-    commands: list[dict[str, str | Callable]] = []
+    commands: list[CommandData] = []
 
     for cmdfile in Path(commands_dir).rglob("*.py"):
         if cmdfile.parent.name != "commands":
@@ -50,17 +180,18 @@ def load_commands(app: Client) -> list[dict[str, str | Callable]]:
             continue
 
         cmd_data: dict = getattr(cmd, "data")
-        log.info(f"Registering command '{cmd_data['name']}'")
+        cmd_data: CommandData = CommandData(getattr(cmd, "data"), cmdfile.name)
+        log.info(f"Registering command '{cmd_data.name}'")
 
         # Collect cmd datas
         commands.append(cmd_data)
 
         # Collect main cmd trigger and its aliases
-        triggers: list[str] = [cmd_data["name"]]
-        if cmd_data.get("alias"):
-            triggers = [*triggers, *cmd_data["alias"]]
+        triggers: list[str] = [cmd_data.name]
+        if cmd_data.alias:
+            triggers = [*triggers, *cmd_data.alias]
 
         # Now register execute function as handler, with main trigger and its aliases, all at once
-        app.add_handler(MessageHandler(cmd_data["execute"], filters.command(triggers)))
+        app.add_handler(MessageHandler(cmd_data.execute, filters.command(triggers)))
 
         return commands
